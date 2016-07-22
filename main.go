65313c907b17b1c4e19a0b6329cbc9487ee767c2
Stevenson Jean-Pierre <stevenson.jean-pierre@sage.com>
2016-07-22 19:18:36 -0400

    command line parsing?

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-//read config/profile files
+//references
+//http://apple.stackexchange.com/questions/128297/how-to-create-a-vpn-connection-via-terminal
+//https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man8/scutil.8.html
+//https://github.com/halo/macosvpn
 
+//todo read config/profile files for connection details
+//todo read vpn host list
+//todo create vpn skeleton
+//todo establish vpn connection
+//todo configure routing
+
+import (
+	"fmt"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"regexp"
+)
+
+var (
+	list = kingpin.Command("list", "List stuff")
+	connect = kingpin.Command("connect", "Connect to a VPN")
+	hosts = list.Command("hosts", "List available vpn hosts")
+	profile = list.Command("profiles", "List available vpn profiles")
+	//profile = connect.Flag("profile", "profile name.").Required().Short('p').String()
+	listRegex = regexp.MustCompile(`^list`)
+)
+
+func listVpnHosts() {
+	fmt.Print("hosts list")
+}
+
+func listVpnProfiles() {
+	fmt.Print("profile list")
+}
+
+func listFunctions(listMethod string) {
+	switch listMethod {
+	case "list hosts":
+		listVpnHosts()
+	case "list profiles":
+		listVpnProfiles()
+	}
+}
 
 func main() {
+	parsed := kingpin.Parse()
+	switch {
+	case listRegex.MatchString(parsed):
+		listFunctions(parsed)
+	}
 }
