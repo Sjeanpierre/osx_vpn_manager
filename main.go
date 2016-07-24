121bb8004e89832b5106ce9c5c8b426a08014ab1
Stevenson Jean-Pierre <stevenson.jean-pierre@sage.com>
2016-07-23 21:08:18 -0400

    hooked up list host command

--- main.go
+++ main.go
@@ -15,19 +15,25 @@ import (
 	"fmt"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"regexp"
+	"os"
+	"log"
+	"path"
 )
 
 var (
+	resourcePath = path.Join(os.Getenv("HOME") ,".vpn_host_manager")
 	connect = kingpin.Command("connect", "Connect to a VPN")
-	//profile = connect.Flag("profile", "profile name.").Required().Short('p').String()
+	profile = connect.Flag("profile", "profile name.").Required().Short('p').String()
 	list = kingpin.Command("list", "List stuff")
 	hosts = list.Command("hosts", "List available vpn hosts")
 	profileList = list.Command("profiles", "List available vpn profiles")
 	listRegex = regexp.MustCompile(`^list`)
+	refreshRegex = regexp.MustCompile(`^refresh`)
+	refresh = kingpin.Command("refresh", "Refreshes resources")
 )
 
 func listVpnHosts() {
-	fmt.Print("hosts list")
+	printHosts()
 }
 
 func listVpnProfiles() {
@@ -43,10 +49,29 @@ func listFunctions(listMethod string) {
 	}
 }
 
+func refreshFunctions(refreshMethod string) {
+	if refreshMethod == "refresh" {
+		refreshHosts()
+	}
+
+}
+
+func setupDirectories() {
+	if _, err := os.Stat(resourcePath); os.IsNotExist(err) {
+		error := os.Mkdir(resourcePath,0644)
+		if error != nil {
+			log.Fatalf("encountered error during setup, %s", error)
+		}
+	}
+}
+
 func main() {
+        setupDirectories()
 	parsed := kingpin.Parse()
 	switch {
 	case listRegex.MatchString(parsed):
 		listFunctions(parsed)
+	case refreshRegex.MatchString(parsed):
+		refreshFunctions(parsed)
 	}
 }
